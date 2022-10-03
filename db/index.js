e49f275000d3a3cf05d355c60de970f40ccc29fb
neliken <nelicacechina@gmail.com>
2022-10-03 13:30:30 +0300

    Updates

--- db/index.js
+++ db/index.js
@@ -1,12 +1,6 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'mi.utm2020',
-    port: 5432,
-});
+const pool = new Pool();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
